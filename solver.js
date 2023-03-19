25ce902d671a8f7489f6104c9149c58310aa52d9
Xiaotian <58612496+Xwang11@users.noreply.github.com>
2023-03-19 17:40:01 -0400

    Completed with for loop

--- solver.js
+++ solver.js
@@ -1,12 +1,22 @@
-while(true) {
-var answerElement = document.querySelector('.answer');
-var problemElement = document.querySelector('.problem');
-var enterEvent = new KeyboardEvent('keydown', {key:'Enter'});
 
-var problem = problemElement.innerHTML;
-var result = eval(problem);
+function solve() {
+    var answerElement = document.querySelector('.answer');
+    var problemElement = document.querySelector('.problem');
+    var enterEvent = new KeyboardEvent('keydown', {key:'Enter'});
 
-answerElement.value = result;
-answerElement.dispatchEvent(enterEvent);
+    var problem = problemElement.innerHTML;
+    var problem = problem.replace(/÷/g, "/");
+    var problem = problem.replace(/–/g, "-");
+    var problem = problem.replace(/×/g, "*");
+    var result = eval(problem);
+
+    answerElement.value = result;
+    answerElement.dispatchEvent(enterEvent);
+}
+
+for(let i = 0; i<69420; i++) {
+    setTimeout(() => {
+        solve();
+      }, 1000);
 }
 
